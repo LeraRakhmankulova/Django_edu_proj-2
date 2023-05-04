9fba5463239c06ca649d074543be52c65e4e4aca
LeraRakhmankulova <lerakhmankulova@mail.ru>
2023-05-04 15:25:29 +0300

    Update settings.py

--- foodcalculate/settings.py
+++ foodcalculate/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'foodcalculate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'food-calculate',
+        'USER': 'postgres',
+        'PASSWORD': 'Le26ra1703.',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
