e8495c99845bb318ae9adac99075600cdb3efa7a
LeraRakhmankulova <lerakhmankulova@mail.ru>
2023-05-08 15:25:11 +0300

    Update admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from web.models import Meal, Product
+
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', "user", 'date')
+    search_fields = ("id", "name")
+    list_filter = "date"
+    ordering = '-date'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'weight', 'fats_count', 'protein_count', 'carbohydrates_count', 'calories_count', 'user')
+    search_fields = ("id", "name")
+    list_filter = "user"
+
+
+admin.site.register(Meal, MealAdmin)
+admin.site.register(Product, ProductAdmin)
